22a290acf1f89f5df45831556fdb3574080f1b75
Evgeniya <evgeniya@ultimatemovers.net>
2022-09-06 12:36:22 +0200

    created global covid chart

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export {default as Cards} from './Cards/Cards';
+export {default as CovidChart} from './CovidChart/CovidChart';
+export {default as CountryPicker} from './CountryPicker/CountryPicker';
